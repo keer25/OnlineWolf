c34a81f264988bc98ff0e0de3a6990e63330ad6d
keer25 <keerukeerthana8@gmail.com>
2015-11-30 18:05:35 +0530

    --

--- OnlineWolf/app/src/main/java/com/example/onlinewolf/onlinewolf/app/MainActivityFragment.java
+++ OnlineWolf/app/src/main/java/com/example/onlinewolf/onlinewolf/app/MainActivityFragment.java
@@ -1,19 +1,24 @@
 package com.example.onlinewolf.onlinewolf.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
  */
+
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
     }
-
+    public void startPlay(View view){
+        Intent login = new Intent(getActivity(), ShowLogin.class);
+        Button button = (Button) view.findViewbyId        }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
