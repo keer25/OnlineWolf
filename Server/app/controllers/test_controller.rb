aa68a6a36501cf1385f9074bd088c5403df29906
KeerthanaS <keerukeerthana8@gmail.com>
2015-12-30 00:52:01 +0530

    ...

--- Server/app/controllers/test_controller.rb
+++ Server/app/controllers/test_controller.rb
@@ -1,25 +1,31 @@
 class TestController < ApplicationController
 	def new
-		EM.run { 
+		@channel = EventMachine::Channel.new
+		EM.run { 	
   			EM::WebSocket.run(:host => "192.168.1.102", :port => 8080) do |ws|
   				logger.info "Waiting for handshake"
     			ws.onopen { |handshake|
       				logger.info "WebSocket connection open"
+      				sid = @channel.subscribe { |msg| ws.send "hello " + msg}
 
       # Access properties on the EM::WebSocket::Handshake object, e.g.
       # path, query_string, origin, headers
 
       # Publish message to the client
-      				ws.send "Hello Client, you connected "
+      				@channel.push "#{sid}"
       				#ws.send "Ignore this: \n\n #{handshake.inspect} \n\n"
-    			}
+    			
 
-    			ws.onclose {  puts "Connection closed" }
+    			ws.onclose do  
+    				logger.info "Connection closed"
+    				@channel.unsubscribe(sid)
+    				end 
 
     			ws.onmessage { |msg|
       				logger.info "Recieved message: #{msg}"
       				ws.send "Pong: #{msg} \n"
     			}
+    		}
   			end
                 }
 	end
