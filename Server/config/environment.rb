8fa59e61eca2e64520f2118bf045d8ba47624f1d
KeerthanaS <keerukeerthana8@gmail.com>
2015-12-31 17:28:59 +0530

    .

--- Server/config/environment.rb
+++ Server/config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-ruby ENV['CUSTOM_RUBY_VERSION'] || '2.2.4'
+#ruby ENV['CUSTOM_RUBY_VERSION'] || '2.2.4'
 
 
 Rails.application.initialize!
